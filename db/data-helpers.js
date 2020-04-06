d43820b8f5e8fdc958b61fb8387f9e4f84e63c3d
introcosm <56694664+introcosm@users.noreply.github.com>
2020-04-06 15:49:48 -0700

    Tests pass (#12)
    * Initial Folders, Files
    
    * The test that passes
    
    * Ensure-auth, part of Poem vertical slice
    
    * Updated app.js
    
    * Passing this off to Poegram Vertical Slice

--- db/data-helpers.js
+++ db/data-helpers.js
@@ -1,24 +1,19 @@
 require('dotenv').config();
-
 const connect = require('../lib/utils/connect');
 const seed = require('../db/seed');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const request = require('supertest');
 const app = require('../lib/app');
-
 beforeAll(() => {
   connect();
 });
-
 beforeEach(() => {
   return mongoose.connection.dropDatabase();
 });
-
 beforeEach(() => {
   return seed();
 });
-
 const agent = request.agent(app);
 beforeEach(() => {
   return agent
@@ -28,14 +23,11 @@ beforeEach(() => {
       password: 'beautiful',
     });
 });
-
 afterAll(() => {
   return mongoose.connection.close();
 });
-
 const prepare = model => JSON.parse(JSON.stringify(model));
 const prepareAll = models => models.map(prepare);
-
 const files = fs.readdirSync('./lib/models');
 const getters = files
   .map(file => require(`../lib/models/${file}`))
@@ -47,7 +39,6 @@ const getters = files
       [`get${Model.modelName}s`]: (query, select) => Model.find(query).select(select).then(prepareAll)
     };
   }, {});
-
 module.exports = {
   ...getters,
   getAgent: () => agent
--- db/seed.js
+++ db/seed.js
@@ -2,15 +2,11 @@ const chance = require('chance').Chance();
 const User = require('../lib/models/User');
 const Poegram = require('../lib/models/Poegram');
 const Poem = require('../lib/models/Poem');
-
-
 module.exports = async({ usersToCreate = 3, poemsToCreate = 10, poegramsToCreate = 50 } = {}) => {
-
   const loggedInUser = await User.create({
     username: 'beauty',
     password: 'beautiful',
   });
-
   const categories = ['Romantic', 'Lake', 'American'];
   const loggedInPoem = await Poem.create({
     author: chance.name(),
@@ -19,20 +15,15 @@ module.exports = async({ usersToCreate = 3, poemsToCreate = 10, poegramsToCreate
     lines: chance.sentence(),
     category: chance.pickone(categories),
   });
-
   await Poegram.create({
     userId: loggedInUser,
     poemId: loggedInPoem,
     colors: [chance.color(), chance.color()]
   });
-
-
-
   const users = await User.create([...Array(usersToCreate)].slice(1).map(() => ({
     username: chance.name(),
     password: chance.animal(),
   })));
-
   // hits external api 
   const poems = await Poem.create([...Array(poemsToCreate)].slice(1).map(() => ({
     author: chance.name(),
@@ -41,7 +32,6 @@ module.exports = async({ usersToCreate = 3, poemsToCreate = 10, poegramsToCreate
     lines: chance.sentence(),
     category: chance.pickone(categories),
   })));
-
   await Poegram.create([...Array(poegramsToCreate)].slice(1).map(() => ({
     userId: chance.pickone(users),
     poemId: chance.pickone(poems),
