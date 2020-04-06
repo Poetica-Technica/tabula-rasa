c332a0175191e651ab9a4efdf58a95bc1884ae44
Nathan Martel <nathanmartel@gmail.com>
2020-04-06 14:45:38 -0700

    Poegram tests passing

--- db/seed.js
+++ db/seed.js
@@ -3,11 +3,28 @@ const User = require('../lib/models/User');
 const Poegram = require('../lib/models/Poegram');
 const Poem = require('../lib/models/Poem');
 
-module.exports = async({ usersToCreate = 5, poemsToCreate = 10, poegramsToCreate = 10 } = {}) => {
+module.exports = async({ usersToCreate = 3, poemsToCreate = 10, poegramsToCreate = 50 } = {}) => {
   const loggedInUser = await User.create({
     username: 'beauty',
     password: 'beautiful',
   });
+
+  const categories = ['Romantic', 'Lake', 'American'];
+  const loggedInPoem = await Poem.create({
+    author: chance.name(),
+    title: chance.name(),
+    text: chance.sentence(),
+    lines: chance.sentence(),
+    category: chance.pickone(categories),
+  });
+
+  await Poegram.create({
+    userId: loggedInUser,
+    poemId: loggedInPoem,
+    colors: [chance.color(), chance.color()]
+  });
+
+
 
   const users = await User.create([...Array(usersToCreate)].slice(1).map(() => ({
     username: chance.name(),
@@ -15,7 +32,6 @@ module.exports = async({ usersToCreate = 5, poemsToCreate = 10, poegramsToCreate
   })));
 
   // hits external api 
-  const categories = ['Romantic', 'Lake', 'American'];
   const poems = await Poem.create([...Array(poemsToCreate)].slice(1).map(() => ({
     author: chance.name(),
     title: chance.name(),
