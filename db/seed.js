59ad3039a70dfc26fad700e7bf71282da4bd5449
Nathan Martel <nathanmartel@users.noreply.github.com>
2020-04-07 17:47:29 -0700

    Nathan (#19)
    * Added poegram model, route, test framework
    
    * Updates for testing
    
    * Poegram tests passing
    
    * Fixes
    
    * Added drawBackground function
    
    * Added CORS
    
    Co-authored-by: introcosm <56694664+introcosm@users.noreply.github.com>

--- db/seed.js
+++ db/seed.js
@@ -2,11 +2,13 @@ const chance = require('chance').Chance();
 const User = require('../lib/models/User');
 const Poegram = require('../lib/models/Poegram');
 const Poem = require('../lib/models/Poem');
+
 module.exports = async({ usersToCreate = 3, poemsToCreate = 10, poegramsToCreate = 50 } = {}) => {
   const loggedInUser = await User.create({
     username: 'beauty',
     password: 'beautiful',
   });
+  
   const categories = ['Romantic', 'Lake', 'American'];
   const loggedInPoem = await Poem.create({
     author: chance.name(),
@@ -15,15 +17,18 @@ module.exports = async({ usersToCreate = 3, poemsToCreate = 10, poegramsToCreate
     lines: chance.sentence(),
     category: chance.pickone(categories),
   });
+
   await Poegram.create({
     userId: loggedInUser,
     poemId: loggedInPoem,
     colors: [chance.color(), chance.color()]
   });
+
   const users = await User.create([...Array(usersToCreate)].slice(1).map(() => ({
     username: chance.name(),
     password: chance.animal(),
   })));
+
   // hits external api 
   const poems = await Poem.create([...Array(poemsToCreate)].slice(1).map(() => ({
     author: chance.name(),
@@ -32,6 +37,7 @@ module.exports = async({ usersToCreate = 3, poemsToCreate = 10, poegramsToCreate
     lines: chance.sentence(),
     category: chance.pickone(categories),
   })));
+
   await Poegram.create([...Array(poegramsToCreate)].slice(1).map(() => ({
     userId: chance.pickone(users),
     poemId: chance.pickone(poems),
